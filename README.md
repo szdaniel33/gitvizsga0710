8b4562218090cbf74fe831a88dc07febe7ed6c8a
Daniel Szalai <szalaidanieldnsn@gmail.com>
2023-07-11 20:07:03 +0200

    .gitignore és README létrehozva, Javascript konzolüzenet hozzáadva

---
+++ README.md
@@ -0,0 +1,5 @@
+git clone https://github.com/szabopeter92/git-vizsga
+cd git-vizsga
+code .
+git branch console
+git checkout console
--- app.js
+++ app.js
@@ -96,3 +96,7 @@ prevBtn.addEventListener("click", prevSong);
 audio.addEventListener("ended", nextSong);
 
 
+window.addEventListener("load", function(){
+
+    console.log("Oldal betöltődése kész")
+})
